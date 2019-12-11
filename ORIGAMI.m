fd761584bf2ae71ef3d8005857715a1d80868c40
Joseph St. Cyr <joseph.a.st.cyr@lmco.com>
2019-12-10 21:17:49 -0500

    -Added CH 8 of Milind Tambe's book which also describes ORIGAMI pseudo-code -Still working on deciphering ORIGAMI pseudo-code...

--- ORIGAMI.m
+++ ORIGAMI.m
@@ -14,7 +14,7 @@
 %   defender_resources - Defender's resources to cover targets
 % 
 % Outputs:
-%   coverage - 
+%   coverage - Defender's coverage vector
 % 
 % -----------------------------------------------------------------------------
 function [coverage] = ORIGAMI(attacker_uncovered_payoff, attacker_covered_payoff, defender_resources)
@@ -32,24 +32,45 @@ function [coverage] = ORIGAMI(attacker_uncovered_payoff, attacker_covered_payoff
     coverage = zeros(num_targets,1);
     added_coverage = zeros(num_targets,1);
 
+    % INITIALIZE THE COVERAGE BOUND (I THINK x = coverage_bound...so put this here for now...)
+    coverage_bound = (coverage(target_index(1))*attacker_covered_payoff(target_index(1))) + ((1-coverage(target_index(1)))*attacker_uncovered_payoff(target_index(1)));
+
+    % INITIALIZE THE RATIO VECTOR
+    ratio = zeros(num_targets,1);
+
     % DETERMINE COVERAGE TO ADD
     t = 1; % t IS THE INDEX INTO THE PAYOFF VECTOR
     while (t <= num_targets-1)
+
         added_coverage(target_index(t)) = ( (attacker_uncovered_payoff(target_index(t+1)) - attacker_uncovered_payoff(target_index(t))) / (attacker_covered_payoff(target_index(t)) - attacker_uncovered_payoff(target_index(t))) ) - coverage(target_index(t));
+
         if ( (coverage(target_index(t)) + added_coverage(target_index(t))) > 1)
             display = fprintf('Coverage probability of target %d exceeded 1 so break.',target_index(t));
             disp(display)
             break;
         end % if
+
         if ( sum(added_coverage(:,1)) > defender_resources_left )
             display = fprintf('Sum of added coverage probability is greater than the remaining defender resources so break. (target = %d)',target_index(t));
             disp(display)
             break;
         end % if
+
         coverage(target_index(t)) = coverage(target_index(t)) + added_coverage(target_index(t));
+
         defender_resources_left = defender_resources_left - sum(added_coverage(:,1));
+
         t = t+1;
     end % while
+
+    ratio(target_index(t)) = (1 / (attacker_uncovered_payoff(target_index(t)) - attacker_covered_payoff(target_index(t))) );
+
+    coverage(target_index(t)) = coverage(target_index(t)) + ( (ratio(target_index(t))*defender_resources_left) / sum(ratio(:,1)));
+
+    if ( coverage(target_index(t)) > 1)
+        display = fprintf('Coverage probability of target %d exceeded 1.',target_index(t));
+        disp(display)
+    end % if
 
 breakpoint = 0; % DUMMY OPERATION TO SET BREAKPOINT
 
