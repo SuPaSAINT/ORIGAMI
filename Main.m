049eb9a326ae812bd6c95f5dff52e84b191e89c1
Joseph St. Cyr <joseph.a.st.cyr@lmco.com>
2019-12-13 08:09:06 -0500

    Main.m -Added cleaner print statements to the command window -Added figure plots
    ORIGAMI.m
    -Used baseline ORIGAMI algorithm and context in paper (specifically the discussion that immediately follows the pseudo-code in the paper up until Theorem 3) to produce what I believe is the true intent of the pseudo-code

--- Main.m
+++ Main.m
@@ -46,102 +46,107 @@ defender_covered_payoff   = [ 10; 20; 30; 40];
 
 config_time = clock;
 config_time_seconds = etime(config_time,start_time);
-config_time_minutes = floor(config_time_seconds/60)
-config_time_seconds = rem(config_time_seconds,60)
+config_time_minutes = floor(config_time_seconds/60);
+config_time_seconds = rem(config_time_seconds,60);
+display = fprintf('Config time (mm:ss): %g:%g.',config_time_minutes,config_time_seconds);
+disp(display)
 
 
 
 % %%%%%%%%%%%%%%%%%% %
 %  EVALUATE GAME #1  %
 % %%%%%%%%%%%%%%%%%% %
+display = fprintf('%%%%% Starting Game 1 %%%%%');
+disp(display)
 [coverage_game1, attacker_payoff_game1, defender_payoff_game1] = ORIGAMI(attacker_uncovered_payoff, attacker_covered_payoff, ...
                                                                          defender_uncovered_payoff, defender_covered_payoff, defender_resources(1));
 
 game_1_time = clock;
 game_1_time_seconds = etime(game_1_time,config_time);
-game_1_time_minutes = floor(game_1_time_seconds/60)
-game_1_time_seconds = rem(game_1_time_seconds,60)
+game_1_time_minutes = floor(game_1_time_seconds/60);
+game_1_time_seconds = rem(game_1_time_seconds,60);
+display = fprintf('Game 1 time (mm:ss): %g:%g.',game_1_time_minutes,game_1_time_seconds);
+disp(display)
 
 
 
 % %%%%%%%%%%%%%%%%%% %
 %  EVALUATE GAME #2  %
 % %%%%%%%%%%%%%%%%%% %
+display = fprintf('%%%%% Starting Game 2 %%%%%');
+disp(display)
 [coverage_game2, attacker_payoff_game2, defender_payoff_game2] = ORIGAMI(attacker_uncovered_payoff, attacker_covered_payoff, ...
                                                                          defender_uncovered_payoff, defender_covered_payoff, defender_resources(2));
 
 game_2_time = clock;
 game_2_time_seconds = etime(game_2_time,game_1_time);
-game_2_time_minutes = floor(game_2_time_seconds/60)
-game_2_time_seconds = rem(game_2_time_seconds,60)
+game_2_time_minutes = floor(game_2_time_seconds/60);
+game_2_time_seconds = rem(game_2_time_seconds,60);
+display = fprintf('Game 2 time (mm:ss): %g:%g.',game_2_time_minutes,game_2_time_seconds);
+disp(display)
 
 
 
 % %%%%%%%%%%%%%%%%%% %
 %  EVALUATE GAME #3  %
 % %%%%%%%%%%%%%%%%%% %
+display = fprintf('%%%%% Starting Game 3 %%%%%');
+disp(display)
 [coverage_game3, attacker_payoff_game3, defender_payoff_game3] = ORIGAMI(attacker_uncovered_payoff, attacker_covered_payoff, ...
                                                                          defender_uncovered_payoff, defender_covered_payoff, defender_resources(3));
 
 game_3_time = clock;
 game_3_time_seconds = etime(game_3_time,game_2_time);
-game_3_time_minutes = floor(game_3_time_seconds/60)
-game_3_time_seconds = rem(game_3_time_seconds,60)
+game_3_time_minutes = floor(game_3_time_seconds/60);
+game_3_time_seconds = rem(game_3_time_seconds,60);
+display = fprintf('Game 3 time (mm:ss): %g:%g.',game_3_time_minutes,game_3_time_seconds);
+disp(display)
 
 
 
 % %%%%%%%%%%%%%%%%%%%%%%% %
 %  GENERATE RESULTS PLOT  %
 % %%%%%%%%%%%%%%%%%%%%%%% %
-% LOOKING INTO THE ORIGINAL FIGURE THAT WAS CREATED THE DATA BEING PLOTTED DOESN'T MAKE SENSE...
-%   EACH GAME PAYOFF IS A ROW VECTOR OF PAYOFF AMOUNTS FOR EACH PLAYER [ATTACKER1, ATTACKER2, DEFENDER] @ THE NASH EQUILIBRIUM...
-%   SO WHY WOULD YOU PLOT EACH GAME PAYOFF ROW VECTOR WITH THE DEFENDER'S AVAILABLE RESOURCES FOR EACH GAME?
-%   THIS DOESN'T MAKE MUCH SENSE SO CREATE DIFFERENT FIGURES THAT SHOW THE DATA IN A LOGICAL MANNER...
-% figure;
-% plot(defender,payoff_1,'-+r');
-% hold on
-% plot(defender,payoff_2,':*g')
-% plot(defender,payoff_3,'-.xk')
-% % legend('Payoff Player 1','Payoff Player 2','Payoff Defender')
-% title('Defender resources vs value of game')
-% xlabel('Defender resources')
-% ylabel('Value of n-person game')
-% hold off
+figure1 = figure;
+    fig1_x_axis = categorical({'Game 1', 'Game 2', 'Game 3'});
+    fig1_x_axis = reordercats(fig1_x_axis,{'Game 1', 'Game 2', 'Game 3'});
+    subplot(2,1,1);
+        fig1_bar1 = bar(fig1_x_axis,defender_resources);
+        legend({'Defender'},'Location','bestoutside');
+        title('Available Resources Per Game');
+        ylabel('Available Resources');
+        fig1_bar1_y_axis_min = 0;
+        fig1_bar1_y_axis_max = max(unique(defender_resources)) + 1;
+        ylim([fig1_bar1_y_axis_min fig1_bar1_y_axis_max]);
+        grid on;
+        box on;
+    subplot(2,1,2);
+        fig1_bar2 = bar(fig1_x_axis,[attacker_payoff_game1, defender_payoff_game1; ...
+                                     attacker_payoff_game2, defender_payoff_game2; ...
+                                     attacker_payoff_game3, defender_payoff_game3]);
+        legend({'Attacker','Defender'},'Location','bestoutside');
+        title('Player Payoff @ Strong Stackelberg Equilibrium');
+        ylabel('Payoff');
+        fig1_bar2_y_axis_min = min(unique([attacker_payoff_game1, defender_payoff_game1; ...
+                                           attacker_payoff_game2, defender_payoff_game2; ...
+                                           attacker_payoff_game3, defender_payoff_game3])) * 1.1;
+        fig1_bar2_y_axis_max = max(unique([attacker_payoff_game1, defender_payoff_game1; ...
+                                           attacker_payoff_game2, defender_payoff_game2; ...
+                                           attacker_payoff_game3, defender_payoff_game3])) * 1.1;
+        ylim([fig1_bar2_y_axis_min fig1_bar2_y_axis_max]);
+        grid on;
+        box on;
 
-% figure1 = figure;
-%   fig1_x_axis = categorical({'Game 1', 'Game 2', 'Game 3'});
-%   fig1_x_axis = reordercats(fig1_x_axis,{'Game 1', 'Game 2', 'Game 3'});
-% 
-%   subplot(2,1,1);
-%     fig1_bar1 = bar(fig1_x_axis,[RESOURCES(1,:); RESOURCES(2,:); RESOURCES(3,:)]);
-%     legend({'Attacker 1','Attacker 2','Defender'},'Location','bestoutside');
-%     title('Player Available Resources Per Game');
-%     ylabel('Available Resources');
-%     fig1_bar1_y_axis_min = 0;
-%     fig1_bar1_y_axis_max = max(unique(RESOURCES)) + 1;
-%     ylim([fig1_bar1_y_axis_min fig1_bar1_y_axis_max]);
-%     grid on;
-%     box on;
-% 
-%   subplot(2,1,2);
-%     fig1_bar2 = bar(fig1_x_axis,[payoff_1; payoff_2; payoff_3]);
-%     legend({'Attacker 1','Attacker 2','Defender'},'Location','bestoutside');
-%     title('Player Payoff @ Nash Equilibrium');
-%     ylabel('Payoff');
-%     fig1_bar2_y_axis_min = 0;
-%     fig1_bar2_y_axis_max = max(unique([payoff_1; payoff_2; payoff_3])) * 1.1;
-%     ylim([fig1_bar2_y_axis_min fig1_bar2_y_axis_max]);
-%     grid on;
-%     box on;
-% 
-% figure2 = figure;
-%   fig2_bar1_x_axis = categorical({'Game 1', 'Game 2', 'Game 3'});
-%   fig2_bar1_x_axis = reordercats(fig2_bar1_x_axis,{'Game 1', 'Game 2', 'Game 3'});
-%   fig2_bar1 = bar(fig2_bar1_x_axis,[etime(game_1_time,config_time); etime(game_2_time,game_1_time); etime(game_3_time,game_2_time)]);
-%   title('Elapsed Time Per Game');
-%   ylabel('Time (Seconds)');
-%   grid on;
-%   box on;
+figure2 = figure;
+    fig2_bar1_x_axis = categorical({'Game 1', 'Game 2', 'Game 3'});
+    fig2_bar1_x_axis = reordercats(fig2_bar1_x_axis,{'Game 1', 'Game 2', 'Game 3'});
+    fig2_bar1 = bar(fig2_bar1_x_axis, [etime(game_1_time,config_time), ...
+                                       etime(game_2_time,game_1_time), ...
+                                       etime(game_3_time,game_2_time)]);
+    title('Elapsed Time Per Game');
+    ylabel('Time (Seconds)');
+    grid on;
+    box on;
 
 
 
@@ -150,7 +155,9 @@ game_3_time_seconds = rem(game_3_time_seconds,60)
 % %%%%%%%%%%%%%%%%%%% %
 stop_time = clock;
 elapsed_seconds = etime(stop_time,start_time);
-elapsed_minutes = floor(elapsed_seconds/60)
-elapsed_seconds = rem(elapsed_seconds,60)
+elapsed_minutes = floor(elapsed_seconds/60);
+elapsed_seconds = rem(elapsed_seconds,60);
+display = fprintf('Total elapsed time (mm:ss): %g:%g.',elapsed_minutes,elapsed_seconds);
+disp(display)
 
 
--- ORIGAMI.m
+++ ORIGAMI.m
@@ -26,12 +26,13 @@
 function [coverage, attacker_payoff, defender_payoff] = ORIGAMI(attacker_uncovered_payoff, attacker_covered_payoff, ...
                                                                 defender_uncovered_payoff, defender_covered_payoff, defender_resources)
     % IF DEBUG_PRINT = 1, THEN DEBUG PRINT STATEMENTS ENABLED, ELSE DISABLED;
-    DEBUG_PRINT = 0;
+    DEBUG_PRINT = 1;
 
     % THE SIZE OF THE VECTOR DETERMINES THE NUMBER OF TARGETS
     num_targets = length(attacker_uncovered_payoff(:,1));
 
-    % NEED TO SORT TARGETS BY MAXIMAL attacker_uncovered_payoff IN DECENDING ORDER
+    % NEED TO SORT TARGETS BY MAXIMAL attacker_uncovered_payoff IN DECENDING ORDER.
+    % NOTE: target_index(t) WILL TRANSLATE THE LOOP INDEX TO TARGET INDEX
     [~,target_index] = sort(attacker_uncovered_payoff, 'descend');
 
     % INITIALIZE REMAINING DEFENDER RESOURCES
@@ -49,64 +50,87 @@ function [coverage, attacker_payoff, defender_payoff] = ORIGAMI(attacker_uncover
     ratio = zeros(num_targets,1);
 
     % INITIALIZE THE FINAL ATTACKER AND DEFENDER PAYOFF VECTORS
-    attacker_payoff = zeros(num_targets,1);
-    defender_payoff = zeros(num_targets,1);
+    attacker_payoff_vector = zeros(num_targets,1);
+    defender_payoff_vector = zeros(num_targets,1);
+    attacker_payoff = 0;
+    defender_payoff = 0;
 
     % DETERMINE COVERAGE TO ADD
-    t = 1; % t IS THE INDEX INTO THE PAYOFF VECTOR
-    while (t <= num_targets-1)
+    next = 2; % next IS THE NEXT TARGET TO ADD TO THE ATTACK VECTOR
+    while (next <= num_targets)
+        for t = 1:next-1 % ENSURE ADDED COVERAGE IS CLEARED FOR 1 TO NEXT AT BEGINNIGN OF EACH LOOP
+            added_coverage(target_index(t)) = 0;
+        end % for
 
-        added_coverage(target_index(t)) = ( (attacker_uncovered_payoff(target_index(t+1)) - attacker_uncovered_payoff(target_index(t))) / (attacker_covered_payoff(target_index(t)) - attacker_uncovered_payoff(target_index(t))) ) - coverage(target_index(t));
+        for t = 1:next-1
+            added_coverage(target_index(t)) = ( (attacker_uncovered_payoff(target_index(next)) - attacker_uncovered_payoff(target_index(t))) / ...
+                                                (attacker_covered_payoff(target_index(t)) - attacker_uncovered_payoff(target_index(t))) ...
+                                              ) - coverage(target_index(t));
 
-        if ((coverage(target_index(t)) + added_coverage(target_index(t))) >= 1)
+            if ((coverage(target_index(t)) + added_coverage(target_index(t))) >= 1)
+                if (DEBUG_PRINT == 1)
+                    display = fprintf('ORIGAMI_MSG01: Target %d.',target_index(t));
+                    disp(display)
+                end % if
+                coverage_bound = max(coverage_bound,attacker_covered_payoff(target_index(t)));
+            end % if
+
+            % NOTE: THAT INEQUALITIES DO NOT MATCH THE PROPOSED PSEUDO-CODE BECAUSE THEY WERE INCORRECT
+            %   COVERAGE BOUND SHOULD CAUSE BREAK IF GREATER THAN THE MINIMAL ATTACKER PAYOFF FOR COVERED TARGET
+            %   IF TOTAL ADDED COVERAGE THIS ITERATION IS > TO REMAINING DEFENDER RESOURCES THEN BREAK
+            if ((coverage_bound > coverage_bound_min) || sum(added_coverage(1:next-1,1)) > defender_resources_left)
+                if (DEBUG_PRINT == 1)
+                    display = fprintf('ORIGAMI_MSG02: Target %d.',target_index(t));
+                    disp(display)
+                end % if
+                break;
+            end % if
+
+            coverage(target_index(t)) = coverage(target_index(t)) + added_coverage(target_index(t));
+
+            defender_resources_left = defender_resources_left - sum(added_coverage(1:next-1,1));
+        end % for
+
+        next = next+1;
+    end % while
+
+    for t = 1:next-1 % COLLECT RATIO FOR EACH TARGET IN THE ATTACK SET TO ALLOCATE REMAINING COVERAGE PROBABILITY
+        ratio(target_index(t)) = (1 / (attacker_uncovered_payoff(target_index(t)) - attacker_covered_payoff(target_index(t))) );
+    end % for
+
+    for t = 1:next-1 % ALLOCATE REMAINING COVERAGE PROBABLITY GIVEN REMAINING DEFENDER RESOURCES AND TARGET RATIOS
+        coverage(target_index(t)) = coverage(target_index(t)) + ( (ratio(target_index(t))*defender_resources_left) / sum(ratio(1:next-1,1)));
+
+        if (coverage(target_index(t)) >= 1)
             if (DEBUG_PRINT == 1)
-                display = fprintf('ORIGAMI_MSG01: Target %d.',target_index(t));
+                display = fprintf('ORIGAMI_MSG03: Target %d.',target_index(t));
                 disp(display)
             end % if
             coverage_bound = max(coverage_bound,attacker_covered_payoff(target_index(t)));
         end % if
 
-        % NOTE: THAT INEQUALITIES DO NOT MATCH THE PROPOSED PSEUDO-CODE BECAUSE THEY WERE INCORRECT
-        %   COVERAGE BOUND SHOULD CAUSE BREAK IF GREATER THAN THE MINIMAL ATTACKER PAYOFF FOR COVERED TARGET
-        %   IF TOTAL ADDED COVERAGE IS >= TO REMAINING DEFENDER RESOURCES THEN BREAK
-        if ((coverage_bound > coverage_bound_min) || sum(added_coverage(:,1)) >= defender_resources_left)
-            if (DEBUG_PRINT == 1)
-                display = fprintf('ORIGAMI_MSG02: Target %d.',target_index(t));
-                disp(display)
-            end % if
-            break;
+        if (coverage_bound > coverage_bound_min)
+            coverage(target_index(t)) = ( (coverage_bound - attacker_uncovered_payoff(target_index(t))) / (attacker_covered_payoff(target_index(t)) - attacker_uncovered_payoff(target_index(t))) )
         end % if
+    end % for
 
-        coverage(target_index(t)) = coverage(target_index(t)) + added_coverage(target_index(t));
-
-        defender_resources_left = defender_resources_left - sum(added_coverage(:,1));
-
-        t = t+1;
-    end % while
-
-    ratio(target_index(t)) = (1 / (attacker_uncovered_payoff(target_index(t)) - attacker_covered_payoff(target_index(t))) );
-
-    coverage(target_index(t)) = coverage(target_index(t)) + ( (ratio(target_index(t))*defender_resources_left) / sum(ratio(:,1)));
-
-    if (coverage(target_index(t)) >= 1)
-        if (DEBUG_PRINT == 1)
-            display = fprintf('ORIGAMI_MSG03: Target %d.',target_index(t));
-            disp(display)
-        end % if
-        coverage_bound = max(coverage_bound,attacker_covered_payoff(target_index(t)));
+    if (DEBUG_PRINT == 1)
+        display = fprintf('Sum(coverage) = %f.',sum(coverage));
+        disp(display)
     end % if
     
-    if (coverage_bound > coverage_bound_min)
-        coverage(target_index(t)) = ( (coverage_bound - attacker_uncovered_payoff(target_index(t))) / (attacker_covered_payoff(target_index(t)) - attacker_uncovered_payoff(target_index(t))) )
+    if (sum(coverage) > defender_resources)
+        display = fprintf('ORIGAMI_ERR01: Total coverage probability is greater than defender_resources!');
+        disp(display)
+        % error('ORIGAMI_ERR01: Total coverage probability is greater than defender_resources!');
     end % if
 
-    % if (DEBUG_PRINT == 1)
-        coverage_sum = sum(coverage);
-    % end % if
-
     for target = 1:num_targets
-        attacker_payoff(target) = (coverage(target)*attacker_covered_payoff(target)) + ((1-coverage(target))*attacker_uncovered_payoff(target));
-        defender_payoff(target) = (coverage(target)*defender_covered_payoff(target)) + ((1-coverage(target))*defender_uncovered_payoff(target));
+        attacker_payoff_vector(target) = (coverage(target)*attacker_covered_payoff(target)) + ((1-coverage(target))*attacker_uncovered_payoff(target));
+        defender_payoff_vector(target) = (coverage(target)*defender_covered_payoff(target)) + ((1-coverage(target))*defender_uncovered_payoff(target));
     end % for
+
+    attacker_payoff = sum(attacker_payoff_vector);
+    defender_payoff = sum(defender_payoff_vector);
 
 end % function
